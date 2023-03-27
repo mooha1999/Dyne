73359a8b55c6e8e26ef8adecfae9f871dd81602b
mooha1999 <50667005+mooha1999@users.noreply.github.com>
2023-03-27 16:49:47 +0200

    add second operation's controller

--- Controllers/check-controller.ts
+++ Controllers/check-controller.ts
@@ -1,22 +1,34 @@
 import { Request, Response } from 'express';
-import * as helpers from './helper-functions';
+import * as itemsHelpers from '../Helpers/items-helper-functions';
+import * as notifyHelpers from '../Helpers/notify-helper-functions';
 
 export async function items(req: Request, res: Response) {
   const { tid: tableID } = req.params;
   try {
-    if (!tableID|| isNaN(Number(tableID))) {
+    if (!tableID || isNaN(Number(tableID))) {
       throw new Error('table id is not provided');
     }
-    const checkNum = await helpers.getCheckNum(tableID);
-    const printedCheck = await helpers.getPrintedCheck(checkNum);
-    const items = helpers.parseItems(printedCheck.slice(5));// Remove the first 5 elements
+    const checkNum = await itemsHelpers.getCheckNum(tableID);
+    const printedCheck = await itemsHelpers.getPrintedCheck(checkNum);
+    const items = itemsHelpers.parseItems(printedCheck.slice(5));// Remove the first 5 elements
     res.status(200).json({ items });
   } catch (error: any) {
-    console.log(error.message);
     res.status(500).json({ message: error.message });
   }
 }
 
-export async function notify(req: Request, res: Response){
-  
+export async function notify(req: Request, res: Response) {
+  const body = req.body;
+  try {
+    const checkInfo = await notifyHelpers.getCheckInfo(body);
+    const isCheckClosed = await notifyHelpers.isCheckClosed(checkInfo);
+    if(!isCheckClosed){
+      throw new Error('Operation failed')
+    }
+    res.status(200).json({message: 'Operation Succeeded :)'})
+  } catch (error: any) {
+    res.status(500).json(error.message);
+  }
+
 }
+
